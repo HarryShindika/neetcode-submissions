b826fd8c8579ed2e906e0416edca1c2f72beb3c1
Harry Shindika <harryshindika@gmail.com>
2026-07-30 18:35:54 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,41 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # 2 pointers, l and r
+        # strip string of empty spaces
+        # while l < r, compare values to each other. if no match, return False else move l and r
+
+        # edge case: len(s) = 1. return True
+
+        if len(s) == 1:
+            return True
+
+        # s = "".join(s.split())
+        # print(s)
+
+        l,r = 0, len(s) - 1
+
+        while l < r:
+
+            c1 = s[l].lower()
+            c2 = s[r].lower()
+
+            if not c1.isalnum() or c1.isspace():
+                l += 1
+                
+            elif not c2.isalnum() or c2.isspace():
+                r -= 1
+                
+            elif c1 != c2:
+                return False
+            else:
+                r -= 1
+                l += 1 
+        
+        return True
+
+        # TIME: O(n), you visit every character in string
+        # SPACE: O(n), join() and split() take linear time
+
+
+
+        
